7053b3a986e366046b2e0d7e622fc8eb4de6f9aa
prozhyvarov <121400043+prozhyvarov@users.noreply.github.com>
2023-08-09 18:22:52 -0600

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {store } from './redux/store';
-import { Provider } from 'react-redux';
+
 import App from 'components/App/App';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
+import { store, persistor } from './redux/store';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-      <App />
+    <PersistGate  loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
