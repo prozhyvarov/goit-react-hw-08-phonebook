240336a40e9014c3ef338e4c2ac9adaa0a8cdc12
prozhyvarov <121400043+prozhyvarov@users.noreply.github.com>
2023-11-01 19:39:25 -0600

    Revert "update project"
    This reverts commit 4b369b48200f1041f5c5c2196623cb459251b0b9.

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -1,6 +1,6 @@
 import { SearchForm } from "components/SearchForm/SearchForm";
 import { LoadMoreButton } from 'components/LoadMoreButton/LoadMoreButton';
-import { NotFound } from "components/NotFound/NotFound";
+// import { NotFound } from "components/NotFound/NotFound";
 import { Container } from "./Catalog.styled";
 import { CarsList } from "components/CarsList/CarsList";
 
